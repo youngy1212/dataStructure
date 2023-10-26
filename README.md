f93703726bba906236ed104a167d012885201b0a
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2023-10-26 17:51:43 +0900

    Two Stack Queue

--- README.md
+++ README.md
@@ -20,3 +20,4 @@
 - [Multi Stack 배열로 구현](./algorithm/Stack&Queue/MultiStack.java)
 - [Stack의 가장 작은 값 찾기(O(1))](./algorithm/Stack&Queue/StackMin.java)
 - [여러 Stack을 이어서 구현](./algorithm/Stack&Queue/SetStack.java)
+- [두 개의 stack으로 queue 구현](./algorithm/Stack&Queue/TwoStackQueue.java)
---
+++ algorithm/Stack&Queue/TwoStackQueue.java
@@ -0,0 +1,62 @@
+import java.util.Stack;
+
+class MyQueue<T>{
+    Stack<T> stackNewest, stackOlddest; //두개의 스택
+
+    MyQueue(){
+        stackNewest = new Stack<T>();
+        stackOlddest =new Stack<T>();
+    }
+
+    public int size(){
+        return stackNewest.size() + stackOlddest.size();
+    }
+
+    //데이터 추가
+    public void add(T value){
+        stackNewest.push(value);
+    }
+
+    //old 스택으로 넘겨주기
+    public void shiftStacks(){
+        if(stackOlddest.isEmpty()){
+            while(!stackNewest.isEmpty()){
+                stackOlddest.push(stackNewest.pop());
+            }
+            //stackNewest 빌때까지 stackNewest로 이동
+        }
+    }
+
+    //oldstack에서 보고, 삭제
+    public T peek(){
+        shiftStacks();
+        return stackOlddest.peek();
+    }
+
+    public T remove(){
+        shiftStacks();
+        return stackOlddest.pop();
+    }
+
+
+
+}
+
+
+
+
+public class TwoStackQueue {
+    
+    public static void main(String[] args) {
+
+        MyQueue<Integer> q = new MyQueue<Integer>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        
+        
+    }
+}
